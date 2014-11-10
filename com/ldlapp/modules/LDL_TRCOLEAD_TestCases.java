54505c8a597d3f8289e119f9d86eeec50fcd6e36
arwin10 <arsoftech@gmail.com>
2014-11-10 18:34:33 +0530

    Nov10Changes3
    6:34pm

--- com/ldlapp/modules/LDL_TRCOLEAD_TestCases.java
+++ com/ldlapp/modules/LDL_TRCOLEAD_TestCases.java
@@ -95,7 +95,7 @@ public class LDL_TRCOLEAD_TestCases {
 		System.out.println("--Inside testCaseExecution function--");
 		//LDLfirstAction();
 	    //LDLsearchRequestverification("LDL_TRCOLEADsearchRequestverification");
-		//LDLHomePageVerification("LDL_TRCOLEAD_HomePageVerification"); /*Issue with Tab scrolling Because of this Testcase failing.*/
+		LDLHomePageVerification("LDL_TRCOLEAD_HomePageVerification"); /*Issue with Tab scrolling Because of this Testcase failing.*/
 		//LDLCreateNewCourseAndOfferingsInstructorLedVerification("LDLTRCOLEAD_CreateNewCourseAndOfferingsInstructorLedVerification");
 		//LDLCreateNewCourseAndOfferingsWBTVerification("LDL_TRCOLEADCreateNewCourseAndOfferingsWBTVerification");
 		//LDLCreateNewCourseAndOfferingsVODVerification("LDL_TRCOLEADCreateNewCourseAndOfferingsVODVerification");
